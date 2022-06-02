9f2099f298673351260ce96fd47edc00590ff5e2
Vladimir Shtefan <shtefanvladimir14@gmail.com>
2022-06-02 21:49:06 +0700

    Refactor comments

--- main.py
+++ main.py
@@ -2,13 +2,23 @@ from os.path import join, abspath
 from flask import Flask
 from functions.functions import load_json
 
+# создаем экземпляр Flask
 app = Flask(__name__)
+# получаем данные из json, предварительно получив путь к файлу
 path_file = abspath(join('files', 'candidates.json'))
 DATA = load_json(path_file)
 
 
 @app.route('/')
 def page_home(answer=''):
+    """
+        функция обработки ответа при переходе на главную страницу
+    Args:
+        answer: ответ на GET /
+
+    Returns:
+        str answer
+    """
     for line in DATA:
         answer += f'Имя кандидата - {line["name"]}\n' \
                   f'Позиция кандидата - {line["position"]}\n' \
@@ -18,6 +28,15 @@ def page_home(answer=''):
 
 @app.route('/candidates/<int:uid>/')
 def page_candidates(uid, answer=''):
+    """
+        функция обработки ответа при переходе на /candidates/<uid>
+    Args:
+        uid: переменная для получения искомого id
+        answer: ответ на GET /candidates/<uid>
+
+    Returns:
+        str answer
+    """
     for line in DATA:
         if line["id"] == uid:
             answer += f'<img src = "www.ya.ru/{uid}">\n\n' \
@@ -29,6 +48,15 @@ def page_candidates(uid, answer=''):
 
 @app.route('/skills/<skill>/')
 def page_skills(skill, answer=''):
+    """
+        функция обработки ответа при переходе на skills/<skill>
+    Args:
+        skill: переменная для получения искомого skill
+        answer: ответ на GET skills/<skill>
+
+    Returns:
+        str answer
+    """
     for line in DATA:
         if skill.lower() in map(str.lower, line["skills"].split(',')):
             answer += f'Имя кандидата - {line["name"]}\n' \
