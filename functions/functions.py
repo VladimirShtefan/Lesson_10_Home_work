2486e321f8fd12698dec39d18cf21e0ab1061cd3
Vladimir Shtefan <shtefanvladimir14@gmail.com>
2022-06-02 22:14:31 +0700

    Refactor functions

--- functions/functions.py
+++ functions/functions.py
@@ -1,7 +1,43 @@
 from json import load
+from os.path import join, abspath
 
 
 def load_json(file_name: str) -> list:
     with open(file_name) as file:
         data = load(file)
     return data
+
+
+path_file = abspath(join('files', 'candidates.json'))
+DATA = load_json(path_file)
+
+
+def get_candidates() -> str:
+    answer = ''
+    for line in DATA:
+        answer += f'Имя кандидата - {line["name"]}\n' \
+                  f'Позиция кандидата - {line["position"]}\n' \
+                  f'Навыки - {line["skills"]}\n\n'
+    return answer
+
+
+def get_candidates_for_id(uid: int) -> str:
+    answer = ''
+    for line in DATA:
+        if line["id"] == uid:
+            answer += f'<img src = "www.ya.ru/{uid}">\n\n' \
+                      f'<pre>\nИмя кандидата - {line["name"]}\n' \
+                      f'Позиция кандидата - {line["position"]}\n' \
+                      f'Навыки - {line["skills"]}\n<pre>'
+            return answer
+    return ''
+
+
+def get_candidates_for_skill(skill: str) -> str:
+    answer = ''
+    for line in DATA:
+        if skill.lower() in map(str.lower, line["skills"].split(',')):
+            answer += f'Имя кандидата - {line["name"]}\n' \
+                      f'Позиция кандидата - {line["position"]}\n' \
+                      f'Навыки - {line["skills"]}\n\n'
+    return answer
--- main.py
+++ main.py
@@ -1,68 +1,45 @@
-from os.path import join, abspath
 from flask import Flask
-from functions.functions import load_json
+from functions.functions import get_candidates, get_candidates_for_id, get_candidates_for_skill
 
 # создаем экземпляр Flask
 app = Flask(__name__)
-# получаем данные из json, предварительно получив путь к файлу
-path_file = abspath(join('files', 'candidates.json'))
-DATA = load_json(path_file)
 
 
 @app.route('/')
-def page_home(answer=''):
+def page_home():
     """
-        функция обработки ответа при переходе на главную страницу
-    Args:
-        answer: ответ на GET /
-
+        Функция обработки ответа при переходе на главную страницу
     Returns:
         str answer
     """
-    for line in DATA:
-        answer += f'Имя кандидата - {line["name"]}\n' \
-                  f'Позиция кандидата - {line["position"]}\n' \
-                  f'Навыки - {line["skills"]}\n\n'
-    return '<pre>\n' + answer + '<pre>'
+    return '<pre>\n' + get_candidates() + '<pre>'
 
 
 @app.route('/candidates/<int:uid>/')
-def page_candidates(uid, answer=''):
+def page_candidates(uid):
     """
         функция обработки ответа при переходе на /candidates/<uid>
     Args:
         uid: переменная для получения искомого id
-        answer: ответ на GET /candidates/<uid>
 
     Returns:
         str answer
     """
-    for line in DATA:
-        if line["id"] == uid:
-            answer += f'<img src = "www.ya.ru/{uid}">\n\n' \
-                     f'<pre>\nИмя кандидата - {line["name"]}\n' \
-                     f'Позиция кандидата - {line["position"]}\n' \
-                     f'Навыки - {line["skills"]}\n<pre>'
-            return answer
+    return get_candidates_for_id(uid)
 
 
 @app.route('/skills/<skill>/')
-def page_skills(skill, answer=''):
+def page_skills(skill):
     """
         функция обработки ответа при переходе на skills/<skill>
     Args:
         skill: переменная для получения искомого skill
-        answer: ответ на GET skills/<skill>
 
     Returns:
         str answer
     """
-    for line in DATA:
-        if skill.lower() in map(str.lower, line["skills"].split(',')):
-            answer += f'Имя кандидата - {line["name"]}\n' \
-                      f'Позиция кандидата - {line["position"]}\n' \
-                      f'Навыки - {line["skills"]}\n\n'
-    return '<pre>\n' + answer + '<pre>'
+    return '<pre>\n' + get_candidates_for_skill(skill) + '<pre>'
 
 
-app.run()
+if __name__ == '__main__':
+    app.run()
